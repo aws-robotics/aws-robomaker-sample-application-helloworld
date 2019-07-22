1d61ab94cd7f45788091220f55111556ab16e4a6
Ross Desmond <44277324+ross-desmond@users.noreply.github.com>
2019-07-22 14:37:55 -0700

    Convert ROS1 hello world to ros2
    * Launch xml schema --> python
    * rotate.py node placed under source with a setup.py entrypoint
    * Removed unnecessary CMakeLists.txt
    * Removed unnecessary requirements from package.xml
    
    Signed-off-by: Ross Desmond <44277324+ross-desmond@users.noreply.github.com>

--- robot_ws/src/hello_world_robot/setup.py
+++ robot_ws/src/hello_world_robot/setup.py
@@ -1,13 +1,39 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
-## See http://ros.org/doc/api/catkin/html/user_guide/setup_dot_py.html
+from setuptools import setup, find_packages
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'hello_world_robot'
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['hello_world_robot'],
-    package_dir={'': 'src'}
+setup(
+    name=package_name,
+    version='1.0.0',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    data_files=[
+        ('share/' + package_name + '/launch', ['launch/rotate.launch.py']),
+        ('share/' + package_name + '/launch', ['launch/deploy_rotate.launch.py']),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='AWS RoboMaker',
+    author_email='ros-contributions@amazon.com',
+    maintainer='AWS RoboMaker',
+    maintainer_email='ros-contributions@amazon.com',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description=(
+        'AWS RoboMaker robot package with a rotating Turtlebot3'
+    ),
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'rotate = hello_world_robot.rotate:main',
+        ],
+    },
 )
 
-setup(**setup_args)
