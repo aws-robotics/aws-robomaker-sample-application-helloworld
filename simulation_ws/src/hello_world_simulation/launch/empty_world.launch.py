36ba1520dfcc3b86fe862e1749d8a67653ed0106
Ross Desmond <44277324+ross-desmond@users.noreply.github.com>
2019-07-31 15:05:10 -0700

    Add all turtlebot3 repos to .rosinstall
    1.
    `cd robot_ws`
    2.
    ```
    rosdep update
    rosws update
    ```
    *Note:* turtlebot3_description CMakeLists.txt has a problematic export dependency on xacro, delete `ament_export_dependency(xacro)` to build
    ```
    rosdep install --from-paths src --ignore-src -r -y
    colcon build
    source install/local_setup.py
    ```
    3.
    `cd ../simulation_wd`
    4. repeat step 2 without the *Note*
    5. `ros2 launch hello_world_simulation empty_world.launch.py`
    6. `ros2 launch hello_world_robot rotate.launch.py`
    
    * turtlebot3 repos are interconnected and require all dependencies to compile through rosws
    
    **TODO:**
    1. Fix turtlebot3_description CMakeLists.txt downstream dependency issue
    2. Modify turtlebot3_description_reduce_mesh `.xacro` files with proper plugin arguments

--- simulation_ws/src/hello_world_simulation/launch/empty_world.launch.py
+++ simulation_ws/src/hello_world_simulation/launch/empty_world.launch.py
@@ -22,11 +22,11 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir
 from launch.actions import ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 
 TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='True')
