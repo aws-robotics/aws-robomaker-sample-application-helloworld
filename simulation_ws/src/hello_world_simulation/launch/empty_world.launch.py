cfc3589e95205cbb595710d2cf75df758535a8ce
Ross Desmond <44277324+ross-desmond@users.noreply.github.com>
2019-07-31 21:18:22 +0000

    Working rotate with reduced mess waffle pi
    Launch files work with `ros2 launch hello_world_simulation empty_world.launch.py`
    Rotation occurs after launching `ros2 launch hello_world_robot rotate.launch.py`
    
    * Requires xacro patch
    * Some plugins not working correctly, under investigation
    
    Signed-off-by: Ross Desmond <44277324+ross-desmond@users.noreply.github.com>

---
+++ simulation_ws/src/hello_world_simulation/launch/empty_world.launch.py
@@ -0,0 +1,56 @@
+# /*******************************************************************************
+# * Copyright 2019 ROBOTIS CO., LTD.
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# *     http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# *******************************************************************************/
+
+# /* Author: Darby Lim */
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir
+from launch.actions import ExecuteProcess
+from launch.substitutions import LaunchConfiguration
+
+TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='True')
+    empty_world_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            get_package_share_directory('gazebo_ros') + '/launch/empty_world.launch.py'))
+    launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_bringup'), 'launch')
+    turtlebot3_description_reduced_mesh_dir = get_package_share_directory('turtlebot3_description_reduced_mesh')
+
+    turtlebot3_description_reduced_mesh_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            turtlebot3_description_reduced_mesh_dir + '/launch/spawn_turtlebot.launch.py'))
+
+    return LaunchDescription([       
+        ExecuteProcess(
+            cmd=['ros2', 'param', 'set', '/gazebo', 'use_sim_time', use_sim_time],
+            output='screen'),
+        ExecuteProcess(
+            cmd=['gzserver', '--verbose', '-s', 'libgazebo_ros_factory.so'],
+            output='screen'
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([launch_file_dir, '/turtlebot3_state_publisher.launch.py']),
+            launch_arguments={'use_sim_time': use_sim_time}.items(),
+        ),
+        turtlebot3_description_reduced_mesh_launch
+    ])
---
+++ simulation_ws/src/turtlebot3_description_reduced_mesh/launch/spawn_turtlebot.launch.py
@@ -0,0 +1,63 @@
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""Launch turtlebot3_description_reduced_mesh and a rotate node."""
+
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))  # noqa
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', 'launch'))  # noqa
+
+import launch
+import tempfile
+from launch_ros import get_default_launch_description
+import launch_ros.actions
+
+from ament_index_python.packages import get_package_share_directory
+import xacro
+import lifecycle_msgs.msg
+import subprocess
+
+def generate_launch_description():
+    """Main."""
+    use_sim_time_false = launch.actions.DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use sim time',
+        )
+    turtlebot3_model = 'waffle_pi'
+    turtlebot3_location = get_package_share_directory('turtlebot3_description_reduced_mesh') \
+        + '/urdf/turtlebot3_' + turtlebot3_model + '.urdf.xacro'
+    with subprocess.Popen(['ros2', 'run', 'xacro', 'xacro', turtlebot3_location], stdout=subprocess.PIPE) as proc:
+        urdf = proc.stdout.read()
+    if urdf is 0:
+        raise 'xacro issue {} return_code={}'.format(turtlebot3_location, urdf)
+    msg = '{name: "robot", xml: "' + ''.join(urdf.decode("utf-8").splitlines()).replace('    ', '')\
+        .replace(r'"', r'\"') + '"}'
+    spawn_robot = launch.actions.ExecuteProcess(
+        cmd=['ros2', 'service', 'call', '/spawn_entity', 'gazebo_msgs/srv/SpawnEntity',
+             msg],
+        output='screen'
+    )
+
+    ld = launch.LaunchDescription(
+        [use_sim_time_false,
+         spawn_robot
+         ])
+    return ld 
+
+
+if __name__ == '__main__':
+    generate_launch_description()
