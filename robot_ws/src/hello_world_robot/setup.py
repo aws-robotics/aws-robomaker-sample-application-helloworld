bead90b6bf89ba5dfcb4a066f82c0e819dc4acc7
AAlon <avishaya@amazon.com>
2019-09-20 22:03:03 +0100

    Add the package to ament's index and bump to 2.0.0 (#34)
    * setup.py - add the package to ament's index and bump to 2.0.0
    
    * Bump version in package.xml to 2.0.0
    
    * Add empty resource file for ament index

--- robot_ws/src/hello_world_robot/setup.py
+++ robot_ws/src/hello_world_robot/setup.py
@@ -4,12 +4,13 @@ package_name = 'hello_world_robot'
 
 setup(
     name=package_name,
-    version='1.0.0',
+    version='2.0.0',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     data_files=[
         ('share/' + package_name + '/launch', ['launch/rotate.launch.py']),
         ('share/' + package_name + '/launch', ['launch/deploy_rotate.launch.py']),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
