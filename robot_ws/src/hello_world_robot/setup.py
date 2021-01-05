66b372959f820c5ac6538923ae622c3ff1523182
ojasjosh <ojasjosh@amazon.com>
2021-01-05 12:32:12 -0800

    Fix linting errors

--- robot_ws/src/hello_world_robot/setup.py
+++ robot_ws/src/hello_world_robot/setup.py
@@ -1,7 +1,8 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
-## See http://ros.org/doc/api/catkin/html/user_guide/setup_dot_py.html
+# ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+# See http://ros.org/doc/api/catkin/html/user_guide/setup_dot_py.html
 
 from distutils.core import setup
+
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
