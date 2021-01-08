9b5d61c5909bd3c9aedc78efb1ab73721d763d1e
ojasjosh <ojasjosh@amazon.com>
2021-01-07 19:34:36 -0800

    Add linter scripts

---
+++ scripts/install_linters.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pip3 install "git+https://github.com/ament/ament_lint.git@0.8.1#egg=ament_copyright-0.8.1&subdirectory=ament_copyright"
+pip3 install "git+https://github.com/ament/ament_lint.git@0.8.1#egg=ament_flake8-0.8.1&subdirectory=ament_flake8"
+pip3 install "git+https://github.com/ament/ament_lint.git@0.8.1#egg=ament_lint-0.8.1&subdirectory=ament_lint"
+pip3 install "git+https://github.com/ament/ament_lint.git@0.8.1#egg=ament_pep257-0.8.1&subdirectory=ament_pep257"
+pip3 install "git+https://github.com/ament/ament_lint.git@0.8.1#egg=ament_xmllint-0.8.1&subdirectory=ament_xmllint"
