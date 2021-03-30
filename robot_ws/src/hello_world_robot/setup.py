0b346bf277c5ae21dd683059aacc51d6e1c0867b
ojasjosh <ojasjosh@amazon.com>
2021-03-30 15:16:44 -0700

    Adding rviz models from turtlebot3_description

--- robot_ws/src/hello_world_robot/setup.py
+++ robot_ws/src/hello_world_robot/setup.py
@@ -2,11 +2,29 @@ from setuptools import find_packages, setup
 
 package_name = 'hello_world_robot'
 
+from ament_index_python.packages import get_package_share_directory
+from setuptools.command.install import install
+from shutil import copyfile
+import os
+
+package_name = 'hello_world_robot'
+
+class CopyRvizModel(install):
+    def run(self):
+        src = get_package_share_directory('turtlebot3_description')+'/rviz/model.rviz'
+        dest_dir = '../../install/hello_world_robot/rviz'
+        os.mkdir(dest_dir)
+        copyfile(src, dest_dir+'/turtlebot3_description.rviz')
+        install.run(self)
+
 setup(
     name=package_name,
     version='2.0.0',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
+    cmdclass={
+        'install': CopyRvizModel,
+    },
     data_files=[
         ('share/' + package_name + '/launch', ['launch/rotate.launch.py']),
         ('share/' + package_name + '/launch', ['launch/deploy_rotate.launch.py']),
