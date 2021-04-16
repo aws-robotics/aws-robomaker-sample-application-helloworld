6435c248d05d1441c2e059a6fd2ec7c81dddc9b0
M. Mei <miaofei@amazon.com>
2021-04-16 00:24:28 -0700

    Add rviz model for ROS2 Foxy (#121)
    * Bump version due to new canonical python patch (#108)
    
    Signed-off-by: Miaofei <miaofei@amazon.com>
    
    * add rviz model -- ROS2 (#113)
    
    * Adding rviz models from turtlebot3_description into robot_ws
    
    Co-authored-by: ojasjosh <ojasjosh@amazon.com>
    Signed-off-by: Miaofei <miaofei@amazon.com>
    
    * Update rviz model name to be consistent with ROS1 (#114)
    
    Co-authored-by: ojasjosh <ojasjosh@amazon.com>
    Signed-off-by: Miaofei <miaofei@amazon.com>
    
    Co-authored-by: Anson Wong <wongtsan@amazon.com>
    Co-authored-by: Ojas Joshi <36244516+ojasjoshi@users.noreply.github.com>
    Co-authored-by: ojasjosh <ojasjosh@amazon.com>

--- robot_ws/src/hello_world_robot/setup.py
+++ robot_ws/src/hello_world_robot/setup.py
@@ -1,17 +1,40 @@
+import os
+from shutil import copyfile
+
+from ament_index_python.packages import get_package_share_directory
 from setuptools import find_packages, setup
+from setuptools.command.install import install
+
 
 package_name = 'hello_world_robot'
 
+
+class CopyRvizModelToPackageDir(install):
+
+    def run(self):
+        rviz_file = get_package_share_directory('turtlebot3_description') \
+            + '/rviz/model.rviz'
+        dest_dir = 'rviz'  # this is relative to hello_world_robot package directory
+        if not os.path.isdir(dest_dir):
+            os.mkdir(dest_dir)
+        copyfile(rviz_file, dest_dir+'/turtlebot3_model.rviz')
+        install.run(self)
+
+
 setup(
     name=package_name,
-    version='2.0.0',
+    version='2.2.1',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
+    cmdclass={
+        'install': CopyRvizModelToPackageDir,
+    },
     data_files=[
         ('share/' + package_name + '/launch', ['launch/rotate.launch.py']),
         ('share/' + package_name + '/launch', ['launch/deploy_rotate.launch.py']),
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('rviz/', ['rviz/turtlebot3_model.rviz'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
