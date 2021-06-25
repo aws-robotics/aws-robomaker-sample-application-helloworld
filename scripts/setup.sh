af0259c8b0a413972e2baadb1b32ef7aa60e83de
chengke <chengke@uefee1b00be3a55.ant.amazon.com>
2021-06-24 18:27:33 -0700

    testing wget fix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -27,9 +27,9 @@ install_ros(){
 
         apt update &&  apt install -y curl gnupg2 lsb-release
         curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
-        curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
+        #curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
-       
+
         apt update
 
         # #Install ROS $ros_distro
@@ -47,7 +47,7 @@ setup_sample_app(){
         #setup key for colcon bundle
         apt-key adv --fetch-keys 'http://packages.osrfoundation.org/gazebo.key'
         apt update
-        apt install -y python3-rosdep git
+        apt install -y python3-rosdep git wget
         if [ ! -f "/etc/ros/rosdep/sources.list.d/20-default.list" ];
         then
                 rosdep init
@@ -67,7 +67,8 @@ setup_sample_app(){
         rosdep install --from-paths src --ignore-src -r -y
         cd ..
 
-        
+        #https://github.com/colcon/colcon-bundle/issues/100
+        wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
 }
 
 if [ -d "/opt/ros" ];
